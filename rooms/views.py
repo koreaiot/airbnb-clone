a9171d40097090331d979c844d7260e156c48d79
koreaiot <jkjgoworld@gmail.com>
2021-06-16 18:45:37 +0900

    # 11.3 page list navigation

--- rooms/views.py
+++ rooms/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()[:5]
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
     return render(request, "rooms/home.html", context={"potato": all_rooms})
