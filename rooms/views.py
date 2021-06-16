be25c37e8ff6e7836932d34b0bb1a92adf45c2c6
koreaiot <jkjgoworld@gmail.com>
2021-06-16 18:31:07 +0900

    #11.1 pagination with offset and limit

--- rooms/views.py
+++ rooms/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    all_rooms = models.Room.objects.all()[:5]
     return render(request, "rooms/home.html", context={"potato": all_rooms})
