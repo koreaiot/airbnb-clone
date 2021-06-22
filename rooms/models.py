13d41bd9d5fccf248e03c4dd6624e882ed5668d3
koreaiot <jkjgoworld@gmail.com>
2021-06-22 16:31:43 +0900

    # 12.2

--- rooms/models.py
+++ rooms/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from core import models as core_models
 from django_countries.fields import CountryField
+from django.urls import reverse
 
 # from users import models as user_models
 
@@ -100,6 +101,9 @@ class Room(core_models.TimeStampedModel):
     def save(self, *args, **kwargs):
         self.city = self.city.title()
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("rooms:detail", kwargs={"pk": self.pk})
 
     def total_rating(self):
         all_reviews = self.reviews.all()
--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,22 @@
 from django.views.generic import ListView
+from django.urls import reverse
+from django.shortcuts import redirect, render
 from . import models
 
 
 class HomeView(ListView):
-    """HomeView Definition"""
-
+    """ HomeView Definition """
     model = models.Room
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
     context_object_name = "rooms"
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
+    
