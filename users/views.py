1ea73534367d0d531a3a65b2fbbb0d18296da246
koreaiot <jkjgoworld@gmail.com>
2021-07-04 15:08:27 +0900

    # 15.0

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django import forms
 from django.views import View
 from django.views.generic import FormView
 from django.urls import reverse_lazy
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import redirect, reverse
 from . import forms
 from django.contrib.auth import authenticate, login, logout
 
@@ -25,3 +25,23 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "KyungJun",
+        "last_name": "Jung",
+        "email": "jkjgoworld@gmail.com",
+    }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
