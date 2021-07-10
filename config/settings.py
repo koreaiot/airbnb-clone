092a3eb4c1e72034bf5b7a373d71c410fa0bdde7
koreaiot <jkjgoworld@gmail.com>
2021-07-10 15:22:34 +0900

    # 16.4 verify email

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,11 @@ AUTH_USER_MODEL = "users.User"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 MEDIA_URL = "/media/"
+
+# Email Configuration
+
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "sexy-guy@sandboxf458cc7a6d2841ac9fcd65e8bb420e6f.mailgun.org"
